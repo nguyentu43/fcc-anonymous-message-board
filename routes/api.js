cc20546973a6f1c4c878ac1941ff8df0ab2ac3b2
Glitch (amenable-acorn) <none>
2019-06-23 13:27:43 +0000

    🐞💍 Checkpoint ./routes/api.js:5024683/541

--- routes/api.js
+++ routes/api.js
@@ -31,7 +31,32 @@ const schemaThread = mongoose.Schema({
     type: Boolean,
     default: false
   },
-  replies:  [{ type: Schema.Types.ObjectId, ref: 'Person' }]
+  board: {
+    type: String,
+    required: true
+  },
+  replies:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }]
+});
+
+const Thread = mongoose.model('Thread', schemaThread);
+
+const schemaReply = mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  delete_password: String,
+  thread_id: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'Reply'
+  },
+  reported: {
+    type: Boolean,
+    default: false
+  },
+  created_on: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD HH:mm:ss')
+  }
 });
 
 module.exports = function (app) {
