9750cb193b651ba0c74a8352b5af758b73e50629
Glitch (amenable-acorn) <none>
2019-06-23 13:16:49 +0000

    🏋🌱 Checkpoint ./routes/api.js:5024683/624 ./package.json:5024683/16789 ./server.js:5024683/166

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,30 @@
 'use strict';
 
 var expect = require('chai').expect;
+const moment = require('moment')l
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB);
+
+const schemaThread = mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  delete_password: String,
+  created_on: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD HH:mm:ss')
+  },
+  bumped_on: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD HH:mm:ss')
+  },
+  reported: {
+    type: Boolean,
+    default: false
+  },
+  replies:  [{ type: Schema.Types.ObjectId, ref: 'Person' }]
+});
 
 module.exports = function (app) {
   
