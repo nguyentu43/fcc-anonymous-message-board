922eed00c6288c9d6e467cfc43ca6501f09acec3
Glitch (amenable-acorn) <none>
2019-06-23 16:42:47 +0000

    🖖👡 Checkpoint ./tests/2_functional-tests.js:5024683/4510

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,7 +24,7 @@ suite('Functional Tests', function() {
         chai
         .request(server)
         .post('/api/threads/test')
-        .send({ text: 'test', delete_password: '1234' })
+        .send({ text: 'test', delete_password: 1234 })
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.text, 'success');
@@ -55,12 +55,50 @@ suite('Functional Tests', function() {
     
     suite('DELETE', function() {
       
-      test('delete threads with password invalid', ())
+      test('delete threads with invalid password ', (done) => {
+    
+        chai
+        .request(server)
+        .delete('/api/threads/test')
+        .send({ delete_password: 1111, thread_id: '5d0fa80fd7e6ea1b928c3cc1'})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+
+      });
+      
+      test('delete threads with valid password ', (done) => {
+    
+        chai
+        .request(server)
+        .delete('/api/threads/test')
+        .send({ delete_password: 1234, thread_id: '5d0fa80fd7e6ea1b928c3cc1'})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+
+      })
       
     });
     
     suite('PUT', function() {
-      
+      test('reported thread', (done) => {
+    
+        chai
+        .request(server)
+        .put('/api/threads/test')
+        .send({thread_id: '5d0fa81e6050b81bfc24f12c'})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+
+      })
     });
     
 
@@ -70,17 +108,87 @@ suite('Functional Tests', function() {
     
     suite('POST', function() {
       
+      test('post reply into thread', (done) => {
+    
+        chai
+        .request(server)
+        .post('/api/replies/test')
+        .send({ thread_id: '5d0fa81e6050b81bfc24f12c', text: 'test', delete_password: 1234})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+
+      })
+      
     });
     
     suite('GET', function() {
+      
+      test('get reply in thread', (done) => {
+        
+        chai
+        .request(server)
+        .get('/api/replies/test')
+        .query({ thread_id: '5d0fa81e6050b81bfc24f12c'})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.replies);
+          done();
+        });
+        
+      });
       
     });
     
     suite('PUT', function() {
       
+      test('report reply in thread', (done) => {
+        
+        chai
+        .request(server)
+        .put('/api/replies/test')
+        .send({ thread_id: , reply_id: })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+        
+      });
+      
     });
     
     suite('DELETE', function() {
+      
+      test('delete reply in thread with invalid password', (done) => {
+        
+        chai
+        .request(server)
+        .delete('/api/replies/test')
+        .send({ thread_id: , reply_id: , delete_password: 1111})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+        
+      });
+      
+      test('delete reply in thread with valid password', (done) => {
+        
+        chai
+        .request(server)
+        .delete('/api/replies/test')
+        .send({ thread_id: , reply_id: , delete_password: 1234})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+        
+      });
       
     });
     
